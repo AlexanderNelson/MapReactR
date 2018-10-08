62354470004a344739ccbf5132516edb1033f21f
Alexander Nelson <amvetserv@gmail.com>
2018-10-08 19:05:39 -0400

    feat: implement foursquare API

---
+++ src/API/index.js
@@ -0,0 +1,51 @@
+class Helper {
+    static baseURL () {
+        return "https://api.foursquare.com/v2";
+    }
+    static auth() {
+        const keys = {
+            client_id: "UBYJXUH5DXAO0XZ2YKVPBPS14PJUVIT112BOYULCBVOEOSG4",
+            client_secret: "UCFQYL03242A1YW3O1QHHBVJKDPTE4A4QJI53LQWMYRN3THP",
+            vers: "20181008"
+        };
+        return Object.keys(keys)
+        .map(key => `${key} = ${keys[key]}`)
+        .join("$");
+    }
+    static urlBuilder(urlParams) {
+        if (!urlParams) {
+            return "";
+        }
+        return Object.keys(urlParams)
+        .map(key => `${key} = ${urlParams[key]}`)
+        .join("&");
+    }
+    static headers() {
+        return {
+            Accept: "application/json"
+        };
+    }
+    static simpleFetch(endPoint, method, urlParams) {
+        let requestData = {
+            method,
+            headers: Helper.headers()
+        };
+        return fetch(
+            `${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(
+            urlParams
+        )}`,
+        requestData
+        ).then(res => res.json());
+    }
+}
+export default class SquareApi {
+    static search(urlParams) {
+        return Helper.simpleFetch("/venues/search", "GET", urlParams);
+    }
+    static getVenueDetails(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
+    }
+    static getVenuePhotos(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SquareAPI from "./API/"
 
 class App extends Component {
+    componentDidMount() {
+      SquareAPI.search({
+        near: "Austin, TX",
+        query: "tacos",
+        limit: 10
+      }).then(results => console.log(results));
+    }
   render() {
     return (
       <div className="App">
