6139f32c99a4bfed4b6e42c44ce6f3b4d92ef86d
Alexander Nelson <amvetserv@gmail.com>
2018-10-23 23:08:32 -0400

    feat: add aria-role to LI(parent)

--- src/component/list-item.js
+++ src/component/list-item.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class ListItem extends Component {
     render() {
         return (
-        <li className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+        <li className="listItem" role="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
         
         <img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name}/>
         <a aria-label={this.props.name}> 
