d35b498f85d4e1d5a4720f75b2707ef5887c158a
Alexander Nelson <amvetserv@gmail.com>
2018-10-25 23:39:04 -0400

    feat: test intent bug in map

--- src/component/Map.js
+++ src/component/Map.js
@@ -51,7 +51,7 @@ const MyMapComponent = withScriptjs(
 );
 
 
-export default class Map extends Component {
+export defalt class Map extends Component {
     render() {
         return (
         <ErrorBoundary>
